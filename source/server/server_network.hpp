44a41149723ef414ebdeae7c6807262e08a605b1
Zohaasad <bscs24135@itu.edu.pk>
2025-11-20 18:18:00 +0500

    Update server_network.hpp

--- source/server/server_network.hpp
+++ source/server/server_network.hpp
@@ -1,18 +1,1 @@
 
-#pragma once
-#include <string>
-#include <vector>
-
-struct ClientRequest {
-    std::string command;
-    std::vector<std::string> args;
-};
-
-struct ServerResponse {
-    bool success;
-    std::string message;
-    std::string data;
-};
-
-ClientRequest parse_request(const std::string& raw);
-std::string serialize_response(const ServerResponse& res);
